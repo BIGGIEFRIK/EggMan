510463940e98dda1f61708d5e4ddb96520885662
BIGGIEFRIK <47160380+BIGGIEFRIK@users.noreply.github.com>
2019-03-22 09:14:08 -0400

    Add files via upload

--- Test/main.lua
+++ Test/main.lua
@@ -3,28 +3,47 @@ function love.load()
   sprite1 = love.graphics.newImage("egg.png-1.png.png")
   sprite2 = love.graphics.newImage("pan.png.png")
   sprite3 = love.graphics.newImage("chicken-1.png.png")
+  sprite4 = love.graphics.newImage("sell.png.png")
   px = 400
   py = 300
   speed = 1
   stamina = 100
   egglimit = 10
-  eggs = 0
+  eggslaid = 0
   eggrate = 1
   time = love.timer.getTime()
   col = false
   eggsheld = 0
+  panlimit = 10
+  eggspan = 0
+  pl = 0
+  eggscooked = 0
 end
 
 function love.update(dt)
   if col == true then
-    eggsheld = eggsheld + eggs
-    eggs = 0
+    eggsheld = eggsheld + eggslaid
+    eggslaid = 0
+  end
+
+  if col1 == true then
+    pl = pl + 1
+    if pl == 1 then
+      if eggsheld > panlimit - eggspan then
+        eggspan = panlimit
+        eggsheld = eggsheld - panlimit
+      else
+        eggspan = eggspan + eggsheld
+        eggsheld = 0
+      end
+    end
   end
 
   col = CheckCollision(90,500,30,60, px,py,50,50)
+  col1 = CheckCollision(40,35,90,30, px,py,50,50)
 
-  if love.timer.getTime() - time > eggrate and eggs < egglimit then
-    eggs = eggs + 1
+  if love.timer.getTime() - time > eggrate and eggslaid < egglimit then
+    eggslaid = eggslaid + 1
     time = love.timer.getTime()
     if stamina < 100 and not love.keyboard.isDown('p') then
       stamina = stamina + 5
@@ -64,13 +83,14 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.print(eggs.."/"..egglimit, 80, 475)
+  love.graphics.print(eggslaid.."/"..egglimit, 80, 475)
+  love.graphics.print(eggspan.."/"..panlimit,90, 30)
   love.graphics.print(px..", "..py..", "..stamina)
   love.graphics.draw(sprite,px,py)
-  love.graphics.draw(sprite1,650,300)
+  love.graphics.draw(sprite4,650,300)
   love.graphics.draw(sprite2,75,55)
   love.graphics.draw(sprite3,75,500)
-  love.graphics.print(tostring(col), 0, 20)
+  love.graphics.print(eggsheld..", "..pl, 0, 20)
 end
 
 function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
